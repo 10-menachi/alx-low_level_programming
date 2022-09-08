93e1cce4920cfdddbdd1ff0ae626a01fc28a8527
chrisdroid <bobitlmr188221@spu.ac.ke>
2022-09-08 21:37:05 +0300

    finish the advanced tasks

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,13 +1,13 @@
 #include <stdio.h>
 /**
  * main - This is the main function
- * Return: 0
+ * Return: 1
  */
 int main(void)
 {
 	/*printing a string to stderr*/
 	fprintf(stderr, "and that piece of art is useful");
 	fprintf(stderr, "\" - Dora Korpar, 2015-10-19\n");
-	return (0);
+	return (1);
 }
 
