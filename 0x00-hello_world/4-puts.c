fbfc937410217d152311c58480d28a2d580310e9
chrisdroid <bobitlmr188221@spu.ac.ke>
2022-09-08 17:24:57 +0300

    finish the mandatory tasks

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/**
+ *  main - This is the main function
+ *
+ *  Return: 0
+ */
+int main(void)
+{
+	/* printing out a string using puts */
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
+
+}
