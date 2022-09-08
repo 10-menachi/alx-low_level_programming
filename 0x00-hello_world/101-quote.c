ec3dfd83ea1497d8250b3d80dfe90cccb671869a
chrisdroid <bobitlmr188221@spu.ac.ke>
2022-09-08 20:44:33 +0300

    finish the advanced tasks

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/**
+ * main - This is the main function
+ * Return: 0
+ */
+int main(void)
+{
+	/*printing a string to stderr*/
+	fprintf("and that piece of art is useful\" - Dora Korpar, 2015-10-19");
+	return (0);
+}
