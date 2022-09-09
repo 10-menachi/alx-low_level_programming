7f15150251e91ec2db2c892726aa269dbdfe9fd7
chrisdroid <bobitlmr188221@spu.ac.ke>
2022-09-09 15:57:18 +0300

    print numberz

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+/**
+ * main - Entry Point
+ *
+ * Return: 0
+ */
+int main(void)
+{
+	int num = 48;
+
+	while (num <= 57)
+	{
+		putchar(num);
+		num++;
+	}
+	putchar(10);
+
+	return (0);
+}
