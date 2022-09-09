d9a6b7ca87665aaa4826846cff367ca40c3be2a3
chrisdroid <bobitlmr188221@spu.ac.ke>
2022-09-09 14:24:56 +0300

    print alphabet

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+/**
+ * main - Entry Point
+ *
+ * Return: 0
+ */
+int main(void)
+{
+	for (int ch = 'a'; ch <= 'z'; ch++)
+	{
+		putchar(ch);
+	}
+	putchar(10);
+
+	return (0);
+}
