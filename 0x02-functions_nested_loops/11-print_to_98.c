938fd4575fd8ef89741b9c6a0014dc9a28a1a4d7
chrisdroid <bobitlmr188221@spu.ac.ke>
2022-09-13 22:10:41 +0300

    print to 98

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -9,7 +9,7 @@ void print_to_98(int n)
 {
 	if (n == 98)
 	{
-		printf("%i", n);
+		printf("%i\n", n);
 	}
 	else if (n > 98)
 	{
@@ -18,6 +18,7 @@ void print_to_98(int n)
 			printf("%i,", n);
 			n--;
 		}
+		printf("\n");
 	}
 	else
 	{
@@ -26,5 +27,6 @@ void print_to_98(int n)
 			printf("%i", n);
 			n++;
 		}
+		printf("\n");
 	}
 }
