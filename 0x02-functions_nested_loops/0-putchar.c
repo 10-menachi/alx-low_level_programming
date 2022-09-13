b7f5b4ca6578fd4b91240e53772c799097388dba
chrisdroid <bobitlmr188221@spu.ac.ke>
2022-09-13 07:37:04 +0300

    put char

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -6,22 +6,15 @@
  */
 int main(void)
 {
-	char *str[] = {};
-	int i = 0;
-	size_t len = sizeof(str) / sizeof(str[0]);
-
-	str[0] = '_';
-	str[1] = 'p';
-	str[2] = 'u';
-	str[3] = 't';
-	str[4] = 'c';
-	str[5] = 'h';
-	str[6] = 'a';
-	str[7] = 'r';
-
-	while (i < len)
-		putchar(str[i]);
-		i++;
+	putchar('_');
+	putchar('p');
+	putchar('u');
+	putchar('t');
+	putchar('c');
+	putchar('h');
+	putchar('a');
+	putchar('r');
+	putchar('\n');
 
 	return (0);
 }
