b48785b108d19e17c54daea879d82f171297b3a2
chrisdroid <bobitlmr188221@spu.ac.ke>
2022-09-14 08:42:50 +0300

    times table

---
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -0,0 +1,46 @@
+#include <stdio.h>
+#include "main.h"
+/**
+ * times_table - prints all the multiples of 9
+ *
+ * Return: void (No return value)
+ */
+void times_table(void)
+{
+	int i = 0, mtp;
+
+	while (i < 10)
+	{
+		int j = 0;
+
+		while (j < 10)
+		{
+			mtp = i * j;
+			if (j == 0)
+			{
+				putchar(mtp + '0');
+				j++;
+			}
+			else if (mtp >= 10)
+			{
+				putchar(' ');
+				putchar(mtp / 10 + '0');
+				putchar(mtp % 10 + '0');
+				j++;
+			}
+			else
+			{
+				putchar(' ');
+				putchar(' ');
+				putchar(mtp + '0');
+				j++;
+			}
+			if (j != 9)
+			{
+				putchar(',');
+			}
+		}
+		putchar('\n');
+		i++;
+	}
+}
