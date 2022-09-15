cb521ad5380d93a2ca8db94101a1be3b2112d1a9
chrisdroid <bobitlmr188221@spu.ac.ke>
2022-09-15 21:04:46 +0300

    print numbers

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ * print_numbers - prints numbers from 0 to 9
+ *
+ * Return: void (No return type)
+ */
+void print_numbers(void)
+{
+	int ch = 48;
+
+	while (ch <= 57)
+		_putchar(ch);
+		ch++;
+	_putchar('\n');
+}
