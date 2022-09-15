c5c6ea981905b2a5d41a83d245b4e52f9e9f3f93
chrisdroid <bobitlmr188221@spu.ac.ke>
2022-09-15 21:01:27 +0300

    multiplication

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,12 @@
+#include "main.h"
+/**
+ * mul - returns the product of two digits
+ * @a: first digit
+ * @b: second digit
+ *
+ * Return: product of the given parameters
+ */
+int mul(int a, int b)
+{
+	return (a * b);
+}
