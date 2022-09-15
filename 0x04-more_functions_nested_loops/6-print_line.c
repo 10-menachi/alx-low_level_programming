8d6f9dc6e56d641fae992cce5dea3021ddff894c
chrisdroid <bobitlmr188221@spu.ac.ke>
2022-09-15 21:36:44 +0300

    print line

---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+/**
+ * print_line - prints a straight line
+ * @n: specifies the length of the line
+ * Return: void (No return value)
+ */
+void print_line(int n)
+{
+	int i = 0;
+
+	while (i <= n)
+	{
+		_putchar('_');
+		i++;
+	}
+}
