017d8cc355c4f7434258de5ca812d7689af4eeb7
chrisdroid <bobitlmr188221@spu.ac.ke>
2022-09-15 21:41:17 +0300

    print line

--- 0x04-more_functions_nested_loops/6-print_line.c
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -7,10 +7,18 @@
 void print_line(int n)
 {
 	int i = 0;
-
-	while (i <= n)
+	
+	if (n <= 0)
 	{
-		_putchar('_');
-		i++;
+		_putchar('0');
 	}
+	else
+	{
+		while (i <= n)
+		{
+			_putchar('_');
+			i++;
+		}
+	}
+	_putchar('\n');
 }
