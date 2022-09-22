e586e89d9ed39b0bac69241ac5c6a61ea59b24ef
chrisdroid <bobitlmr188221@spu.ac.ke>
2022-09-22 07:05:19 +0300

    strncat

--- 0x06-pointers_arrays_strings/1-strncat.c
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -9,7 +9,8 @@
  */
 char *_strncat(char *dest, char *src, int n)
 {
-	int i, j = 0;
+	int i = 0;
+	int j = 0;
 
 	while (dest[i] != '\0')
 	{
@@ -23,8 +24,11 @@ char *_strncat(char *dest, char *src, int n)
 		{
 			break;
 		}
-		i++;
-		j++;
+		else
+		{
+			i++;
+			j++;
+		}
 	}
 	return (dest);
 }
