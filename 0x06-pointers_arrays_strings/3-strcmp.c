65a9747c5b4544df64f090ca50bb55f4ff1719c9
chrisdroid <bobitlmr188221@spu.ac.ke>
2022-09-22 07:09:15 +0300

    strcmp

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -4,4 +4,26 @@
  * @s1: first string
  * @s2: second string
  *
- * Return: 0 if they are 
+ * Return: 0 if they are equal,
+ *  any other integer otherwise
+ */
+int _strcmp(char *s1, char *s2)
+{
+	int i = 0;
+	int equal = 0;
+
+	while (equal = 0)
+	{
+		if (s1[i] == '\0' && s2[i] == '\0')
+		{
+			break;
+		}
+		else
+		{
+			equal = s1[i] - s2[i];
+			i++;
+		}
+	}
+
+	return (op);
+}
