a0e68c2ea1378e41907d707d3a26711f0cce8f7b
chrisdroid <bobitlmr188221@spu.ac.ke>
2022-09-26 16:08:46 +0300

    str chr

--- 0x07-pointers_arrays_strings/2-strchr.c
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -9,22 +9,16 @@
  */
 char *_strchr(char *s, char c)
 {
-	void *st;
 	unsigned int i = 0;
+	char *ptr = s;
 
-	while (*(s + i) != '\0')
+	while (*(ptr + i) >= '\0')
 	{
-		if (c == *(s + i))
+		if (*(ptr + i) == c)
 		{
-			return (s + i);
+			return (ptr + i);
 		}
+		i++;
 	}
-	if (*(s + i) == c)
-	{
-		return (s + i);
-	}
-	else
-	{
-		return ('\0');
-	}
+	return ('\0');
 }
