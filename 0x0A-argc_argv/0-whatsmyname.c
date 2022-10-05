9501702851634ca4a94524a0097f890802be56f7
chrisdroid <bobitlmr188221@spu.ac.ke>
2022-10-05 18:34:21 +0300

    whats my name

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,17 @@
+#include "stdio.h"
+
+/**
+ * main - Entry point
+ * 
+ * @argc: number of arguments
+ * @argv: pointer to arguments array
+ */
+
+int main(int argc, char *argv[])
+{
+	if (argc > 0)
+	{
+		printf("%s\n", argv[0]);
+	}
+	return (0);
+}
---
+++ 0x0A-argc_argv/README.md
@@ -0,0 +1,4 @@
+# 0x0A. C - argc, argv
+
+## 0-whatsmyname.c
+#### prints the program's name followed by a new line
