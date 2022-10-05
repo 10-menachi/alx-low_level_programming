d95db7f5496c7fedbba80f6cd23cbb1994ef936e
chrisdroid <bobitlmr188221@spu.ac.ke>
2022-10-05 04:05:33 +0300

    str concat

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -25,17 +25,17 @@ char *str_concat(char *s1, char *s2)
 	while (s1[i] || s2[i])
 		j++;
 		i++;
-
 	s3 = malloc(sizeof(char) * j);
 	if (s3 == NULL)
 		return (NULL);
 
 	i = 0;
+
 	while (s1[i])
 		s3[k++] = s1[i];
 		i++;
-
 	i = 0;
+
 	while (s2[i])
 		s3[k++] = s2[i];
 		i++;
