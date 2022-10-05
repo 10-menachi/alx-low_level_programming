5af4fa7250ea3b8eb77c8361e68a79ff744cdca0
chrisdroid <bobitlmr188221@spu.ac.ke>
2022-10-05 18:42:10 +0300

    argc

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,13 @@
+#include "stdio.h"
+/**
+ * main - entry point
+ * @argc: number of arguments
+ * @argv: pointer to arguments array
+ *
+ * Return: 0
+ */
+int main(int argc, char *argv[])
+{
+	printf("%d\n", argc);
+	return (0);
+}
--- 0x0A-argc_argv/README.md
+++ 0x0A-argc_argv/README.md
@@ -2,3 +2,6 @@
 
 ## 0-whatsmyname.c
 #### prints the program's name followed by a new line
+
+## 1-args.c
+#### prints the number of arguments passed to it
