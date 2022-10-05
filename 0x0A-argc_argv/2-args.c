5f823a9509beab80716915e4b521d822091b9bf7
chrisdroid <bobitlmr188221@spu.ac.ke>
2022-10-05 18:52:03 +0300

    2 args

---
+++ 0x0A-argc_argv/2-args.c
@@ -0,0 +1,22 @@
+#include "stdio.h"
+/**
+ * main - entry point
+ * @argc: number of arguments
+ * @argv: pointer to array of arguments
+ *
+ * Return: 0
+ */
+int main(int argc, char *argv[])
+{
+	int i;
+
+	if (argc > 0)
+	{
+		i = 0;
+		while (i < argc)
+		{
+			printf("%s\n", argv[i]);
+			i++;
+		}
+	}
+}
