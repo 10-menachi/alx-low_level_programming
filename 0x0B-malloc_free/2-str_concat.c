199116604668b842a1a89571ea6004d86fb56c1c
chrisdroid <bobitlmr188221@spu.ac.ke>
2022-10-05 04:01:32 +0300

    str concat

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -17,22 +17,28 @@ char *str_concat(char *s1, char *s2)
 
 	if (s1 == NULL)
 		s1 = "";
+
 	if (s2 == NULL)
 		s2 = "";
+
 	i = 0;
 	while (s1[i] || s2[i])
 		j++;
 		i++;
+
 	s3 = malloc(sizeof(char) * j);
 	if (s3 == NULL)
 		return (NULL);
+
 	i = 0;
 	while (s1[i])
 		s3[k++] = s1[i];
 		i++;
+
 	i = 0;
 	while (s2[i])
 		s3[k++] = s2[i];
 		i++;
+
 	return (s3);
 }
