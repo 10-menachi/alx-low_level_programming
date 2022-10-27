52a21205d55c8bf78a4d9a795c7fbeb94ed459da
chrisdroid <bobitlmr188221@spu.ac.ke>
2022-10-27 22:44:50 +0300

    delete node int

--- 0x13-more_singly_linked_lists/10-delete_nodeint.c
+++ 0x13-more_singly_linked_lists/10-delete_nodeint.c
@@ -5,7 +5,6 @@
  * position
  * @head: head of the list
  * @index: position to the to be deleted node
- * 
  * Return: 1 if process is successful, -1
  * if process fails
  */
@@ -22,7 +21,7 @@ int delete_nodeint_at_index(listint_t **head, unsigned int index)
 	{
 		*head = temp->next;
 		free(temp);
-		return 1;
+		return (1);
 	}
 	while (i < index - 1 && temp != NULL)
 	{
