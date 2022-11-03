ef1cc410342e11ec06c1bc864644822c31045b7b
chrisdroid <bobitlmr188221@spu.ac.ke>
2022-11-03 10:43:17 +0300

    read text file

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -1,28 +1,30 @@
 #include "main.h"
-
 /**
- * read_textfile - read a certain size and prints it to
- * stdout
+ * read_textfile - read a certain size and prints to std output
  * @filename: file to read from
  * @letters: size to read
- *
  * Return: actual size read and printed
  */
-
 ssize_t read_textfile(const char *filename, size_t letters)
 {
-	int fd;
+
+	int fd; /* file descriptor */
 	ssize_t n_read, n_wrote;
-	char buffer;
+	char *buffer;
 
 	if (filename == NULL)
 		return (0);
+
+	/* open */
 	fd = open(filename, O_RDONLY);
 	if (fd == -1)
 		return (0);
+	/* malloc buffer */
 	buffer = malloc(sizeof(char) * letters);
 	if (buffer == NULL)
 		return (0);
+
+	/* read */
 	n_read = read(fd, buffer, letters);
 	if (n_read == -1)
 	{
@@ -30,6 +32,8 @@ ssize_t read_textfile(const char *filename, size_t letters)
 		close(fd);
 		return (0);
 	}
+
+	/* write */
 	n_wrote = write(STDOUT_FILENO, buffer, n_read);
 	if (n_wrote == -1)
 	{
@@ -37,6 +41,7 @@ ssize_t read_textfile(const char *filename, size_t letters)
 		close(fd);
 		return (0);
 	}
+
 	close(fd);
 	return (n_read);
 }
