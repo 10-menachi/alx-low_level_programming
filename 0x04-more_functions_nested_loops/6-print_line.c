a466a246a0b01eda66e524565e35088247433ad0
10-menachi <bobitlmr188221@spu.ac.ke>
2022-12-11 11:36:09 +0300

    6-print_line

--- 0x04-more_functions_nested_loops/6-print_line.c
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -1,17 +1,19 @@
 #include "main.h"
+
 /**
  * print_line - prints a straight line
  * @n: specifies the length of the line
  * Return: void (No return value)
  */
+
 void print_line(int n)
 {
-	if (n <= 0)
+	if (n > 0)
 	{
-		_putchar(10);
+		for (int i = 0; i <= n; i++)
+		{
+			_putchar('_');
+		}
 	}
-	for (int i = 0; i <= n; i++)
-	{
-		_putchar('_');
-	}
+	_putchar('\n');
 }
