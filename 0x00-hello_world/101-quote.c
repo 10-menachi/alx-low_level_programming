4f230cb633333fd735c194d9d48403f40b4c41d0
chrisdroid <bobitlmr188221@spu.ac.ke>
2023-02-16 21:07:52 +0300

    101-quote.c

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,4 +1,5 @@
-#include "stdio.h"
+#include "string.h"
+#include "unistd.h"
 
 /**
  * main - entry point
@@ -9,7 +10,7 @@
 
 int main(void)
 {
-	fprintf(stderr, "and that piece of art is useful\"
-			- Dora Korpar, 2015-10-19");
+	char *s = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
+	write(2, s, strlen(s));
 	return (0);
 }
