a5ddbc38112a1b8aeaae65528c0b3e99be85ebfc
chrisdroid <bobitlmr188221@spu.ac.ke>
2023-02-27 19:39:36 +0300

    puts 2

--- 0x05-pointers_arrays_strings/6-puts2.c
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -8,12 +8,11 @@
 
 void puts2(char *str)
 {
-	int i = 0;
+	int i, len = 0;
 
-	while (*(str + i))
-	{
-		_putchar(*(str + i));
-		i += 2;
-	}
+	while (*str++)
+		len++;
+	for (i = 0; i < len; i += 2)
+		_putchar(str[i]);
 	_putchar(10);
 }
