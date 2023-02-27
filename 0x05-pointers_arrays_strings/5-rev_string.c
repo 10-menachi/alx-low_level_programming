9777c6c2c53bbc4055343c33065b8e45d6d7ac32
chrisdroid <bobitlmr188221@spu.ac.ke>
2023-02-27 19:23:21 +0300

    rev string

--- 0x05-pointers_arrays_strings/5-rev_string.c
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -7,8 +7,16 @@
 
 void rev_string(char *s)
 {
-	while (*s++)
-	{
+	char temp;
+	int a, len1 = 0, len2 = 0;
 
+	while (s[len1] != '\0')
+		len++;
+	len2 = len1 - 1;
+	for (a = 0; a < len / 2; a++)
+	{
+		temp = s[a];
+		s[a] = s[len2];
+		s[len2--] = temp;
 	}
 }
