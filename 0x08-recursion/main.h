6818deccbe283e4fb5957646ad1ea19ac5136126
chrisdroid <bobitlmr188221@spu.ac.ke>
2023-03-09 17:43:25 +0300

    strlen recursion

--- 0x08-recursion/main.h
+++ 0x08-recursion/main.h
@@ -5,5 +5,7 @@ int _putchar(int ch);
 int factorial(int n);
 int _pow_recursion(int x, int y);
 void _puts_recursion(char *s);
+void _print_rev_recursion(char *s);
+void _print_rev_recursion(char *s);
 
 #endif
