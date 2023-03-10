9493c8c6dbe7e8827154442265817ce0f4b9d107
chrisdroid <bobitlmr188221@spu.ac.ke>
2023-03-10 18:07:16 +0300

    2 args

--- 0x0A-argc_argv/2-args.c
+++ 0x0A-argc_argv/2-args.c
@@ -8,13 +8,13 @@
  * Return: 0
  */
 
-int main(int argc, int *argv[])
+int main(int argc, char *argv[])
 {
 	int i;
 
 	for (i = 0; i < argc; i++)
 	{
-		printf("%s", argv[i]);
+		printf("%s\n", argv[i]);
 	}
 	return (0);
 }
