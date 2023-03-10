89412dab1ce9eb05bc4e2122dcb120377cf65c7b
chrisdroid <bobitlmr188221@spu.ac.ke>
2023-03-10 17:52:42 +0300

    args

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * main - entry point
+ * @argc: argument count
+ * @argv: arguments array
+ *
+ * Return: 0
+ */
+
+int main(int argc, char *argv)
+{
+	printf("%i\n", argc);
+	return (0);
+}
