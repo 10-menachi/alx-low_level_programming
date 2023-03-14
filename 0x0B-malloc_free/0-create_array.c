744546f20953d24a2abacb5fbd0e7d7ab3894411
chrisdroid <bobitlmr188221@spu.ac.ke>
2023-03-14 20:36:40 +0300

    create array

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -13,10 +13,12 @@
 char *create_array(unsigned int size, char c)
 {
 	char *arr;
+	int i;
 
 	if (size == 0)
 		return (NULL);
 	arr = malloc(sizeof(char) * size);
-	arr[0] = c;
+	for (i = 0; i <= size; i++)
+		arr[i] = c;
 	return (arr);
 }
