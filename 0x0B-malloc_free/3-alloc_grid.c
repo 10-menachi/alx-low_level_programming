8fd4ffec26dfb4e89b84f36064d0a31bc74608ac
chrisdroid <bobitlmr188221@spu.ac.ke>
2023-03-15 13:29:52 +0300

    free grid

--- 0x0B-malloc_free/3-alloc_grid.c
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -1,4 +1,16 @@
 #include "main.h"
 
 /**
+ * alloc_grid - allocates a grid
+ * @width: width
+ * @height: height
  *
+ * Return: pointer to grid, NULL if process fails;
+ */
+
+int **alloc_grid(int width, int height)
+{
+	if (width <= 0 || height <= 0)
+		return (NULL);
+	
+}
