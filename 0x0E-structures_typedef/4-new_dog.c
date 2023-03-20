306849117f5ce0f240a2082dd491c5e1bc6a1a67
chrisdroid <bobitlmr188221@spu.ac.ke>
2023-03-20 19:21:32 +0300

    created a function that creates a new dog

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -11,7 +11,12 @@
 
 dog_t *new_dog(char *name, float age, char *owner)
 {
-	dog_t d = { name, age, owner };
+	dog_t *d;
 
-	return (*d);
+	d = malloc(sizeof(dog_t));
+	d->name = name;
+	d->age = age;
+	d->owner = owner;
+
+	return (d);
 }
