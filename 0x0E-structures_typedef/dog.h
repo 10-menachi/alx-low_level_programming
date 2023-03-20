9e994459bdb04c03a1a5bb618d1d8bce8d394c94
chrisdroid <bobitlmr188221@spu.ac.ke>
2023-03-20 15:19:38 +0300

    create a struct called dog

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,18 @@
+#ifndef DOG_H
+#define DOG_H
+
+/**
+ * struct dog - structure representation of a dog
+ * @name: name of the dog
+ * @owner: name of the owner of the dog
+ * @age: age of the dog
+ */
+
+struct dog
+{
+	char *name;
+	int age;
+	char *owner;
+};
+
+#endif
