1b5199c9d8ef345637c01d782c980eeeaad1df3d
chrisdroid <bobitlmr188221@spu.ac.ke>
2023-03-24 13:11:51 +0300

    Edit variadic_functions.h header file

--- 0x10-variadic_functions/variadic_functions.h
+++ 0x10-variadic_functions/variadic_functions.h
@@ -1,5 +1,7 @@
-#ifndef MAIN_H
-#define MAIN_H
+#ifndef VARIADIC_FUNCTIONS_H
+#define VARIADIC_FUNCTIONS_H
+
+#include <stdarg.h>
 
 int _putchar(int ch);
 int sum_them_all(const unsigned int n, ...);
