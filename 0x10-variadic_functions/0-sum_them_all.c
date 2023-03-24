320d4e8bee18762408a347de01b4f11fd5ec0c8e
chrisdroid <bobitlmr188221@spu.ac.ke>
2023-03-24 13:18:40 +0300

    Fix bug in sum_them_all function

--- 0x10-variadic_functions/0-sum_them_all.c
+++ 0x10-variadic_functions/0-sum_them_all.c
@@ -10,14 +10,19 @@
 int sum_them_all(const unsigned int n, ...)
 {
 	va_list arg_ptr;
-	unsigned int i, sum = 0;
+	unsigned int i;
+	int sum, var;
 
 	va_start(arg_ptr, n);
+
 	if (n == 0)
 		return (0);
 
 	for (i = 0; i < n; i++)
-		sum += va_arg(arg_ptr, int);
+	{
+		var = va_arg(arg_ptr, int);
+		sum += var;
+	}
 	va_end(arg_ptr);
 	return (sum);
 }
