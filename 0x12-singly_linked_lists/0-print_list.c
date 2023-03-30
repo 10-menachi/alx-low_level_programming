7a54c895528dcd95b5d3e2aac100108acf502275
chrisdroid <bobitlmr188221@spu.ac.ke>
2023-03-30 13:42:21 +0300

    Fix bug in print_list function

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -11,9 +11,12 @@ size_t print_list(const list_t *h)
 {
 	size_t elements = 0;
 
-	while (h != NULL)
+	while (h)
 	{
-		printf("%s\n", h->str);
+		if (h->str)
+			printf("[%d] %s\n", h->len, h->str);
+		else
+			printf("[0] (nil)\n");
 		h = h->next;
 		elements++;
 	}
