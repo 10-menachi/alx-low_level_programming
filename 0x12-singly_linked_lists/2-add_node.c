4fd6d16540e3f204d41d8d838300ea8d0ad785d6
chrisdroid <bobitlmr188221@spu.ac.ke>
2023-03-30 15:07:53 +0300

    Fix linter error

--- 0x12-singly_linked_lists/2-add_node.c
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -21,14 +21,16 @@ list_t *add_node(list_t **head, const char *str)
 		return (NULL);
 	dup = strdup(str);
 	if (!dup)
+	{
 		free(new);
 		return (NULL);
+	}
 	while (str[len])
 		len++;
 
 	new->str = dup;
 	new->len = len;
-	new -> next = *head;
+	new->next = *head;
 	*head = new;
 
 	return (new);
