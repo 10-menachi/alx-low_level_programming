2d1ba4e3e37abcaed368175da1ce5351e647e78e
chrisdroid <bobitlmr188221@spu.ac.ke>
2023-03-30 13:29:01 +0300

    Implement print_list function

---
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -0,0 +1,22 @@
+#include "lists.h"
+
+/**
+ * print_list - prints all the elements of a list_t list
+ * @h: given list
+ *
+ * Return: number of nodes
+ */
+
+size_t print_list(const list_t *h)
+{
+	int elements = 0;
+
+	while (h != NULL)
+	{
+		printf("%s", h->next->str);
+		h = h->next;
+		elements++;
+	}
+
+	return (elements);
+}
