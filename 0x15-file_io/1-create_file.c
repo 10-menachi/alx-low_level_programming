7acc068f6949f5e688a6783c94545fc7a0693f22
chrisdroid <bobitlmr188221@spu.ac.ke>
2023-04-11 09:04:32 +0300

    Fix bug in create_file function

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -15,7 +15,7 @@ int create_file(const char *filename, char *text_content)
 
 	if (!filename)
 		return (-1);
-	fd = open(filename, O_RDWR);
+	fd = open(filename, O_CREAT | O_RDWR, 0600);
 	if (fd == -1)
 		return (-1);
 	len = strlen(text_content);
