07481ada40b7c613aa9854e112c2f5e76a21da68
chrisdroid <bobitlmr188221@spu.ac.ke>
2023-04-23 16:12:41 +0300

    Implement binary_to_uint function

---
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -0,0 +1,66 @@
+#include "main.h"
+
+/**
+ * str_len - Finds length of string.
+ * @str: String of which length is to be found.
+ *
+ * Return: The length of the string;
+ */
+int str_len(const char *str)
+{
+	int len = 0, index;
+
+	for (index = 0; *(str + index); index++)
+	{
+		if (*(str + index) == '\0')
+			break;
+		len++;
+	}
+	return (len);
+}
+
+/**
+ * pow - Finds Power @y of @x.
+ * @x: Base number.
+ * @y: Exponent.
+ *
+ * Return: x raised to power y.
+ */
+int pow(int x, int y)
+{
+	int p = 0;
+
+	if (y == 0)
+		return (1);
+	p = x * pow(x, y - 1);
+	return (p);
+}
+
+/**
+ * binary_to_uint - Converts binary number to unsigned int.
+ *
+ * @b: Pointer to string of 0 and 1 chars.
+ *
+ * Return: The converted number, 0 if b is NULL.
+ *	   0 if there's one or more chars in b != 0 or 1.
+ */
+unsigned int binary_to_uint(const char *b)
+{
+	unsigned int len = str_len(b);
+	unsigned int d = 0, index = 0;
+
+	if (b == NULL)
+		return (0);
+
+	while (len > 0)
+	{
+		--len;
+		if (*(b + len) != '0' && *(b + len) != '1')
+		{
+			return (0);
+		}
+		d += (*(b + len) - '0') * pow(2, index);
+		index++;
+	}
+	return (d);
+}
