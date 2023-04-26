89659f10b92945654a0524d332969bba6487cd1a
chrisdroid <bobitlmr188221@spu.ac.ke>
2023-04-26 12:28:16 +0300

    Fix bug in code

--- 0x15-file_io/100-elf_header.c
+++ 0x15-file_io/100-elf_header.c
@@ -1,94 +1,71 @@
-#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <fcntl.h>
+#include <unistd.h>
+#include <elf.h>
 
 /**
- * print_error - prints to stderr
- * @msg: error message
+ * print_error - prints an error message to stderr and exits with status 98
+ * @msg: the error message to print
  */
 void print_error(char *msg)
 {
-    fprintf(stderr, "Error: %s\n", msg);
+	fprintf(stderr, "Error: %s\n", msg);
+	exit(98);
 }
 
 /**
- * print_header - prints header
- * @header: given header
+ * print_header - prints the ELF header
+ * @header: a pointer to the ELF header structure
  */
 void print_header(Elf32_Ehdr *header)
 {
-    int i;
+	int i;
 
-    printf("ELF Header:\n");
-    printf("  Magic:   ");
-    for (i = 0; i < EI_NIDENT; i++)
-    {
-        printf("%02x ", header->e_ident[i]);
-    }
-    printf("\n");
-    printf(
-        "  Class:                             ELF%d\n"
-        "  Data:                              2's complement, little endian\n"
-        "  Version:                           %d (current)\n"
-        "  OS/ABI:                            UNIX - %s\n"
-        "  ABI Version:                       %d\n"
-        "  Type:                              EXEC (Executable file)\n"
-        "  Entry point address:               0x%x\n",
-        header->e_ident[EI_CLASS] == ELFCLASS64 ? 64 : 32,
-        header->e_ident[EI_VERSION],
-        header->e_ident[EI_OSABI] == ELFOSABI_NONE ? "UNIX" :
-            header->e_ident[EI_OSABI] == ELFOSABI_NETBSD ? "NetBSD" :
-            "Unknown",
-        header->e_ident[EI_ABIVERSION],
-        header->e_entry
-    );
+	printf("ELF Header:\n");
+	printf("  Magic:   ");
+	for (i = 0; i < EI_NIDENT; i++)
+		printf("%02x ", header->e_ident[i]);
+	printf("\n");
+	printf("  Class:                             ELF32\n");
+	printf("  Data:                              2's complement, little endian\n");
+	printf("  Version:                           1 (current)\n");
+	printf("  OS/ABI:                            UNIX - NetBSD\n");
+	printf("  ABI Version:                       0\n");
+	printf("  Type:                              EXEC (Executable file)\n");
+	printf("  Entry point address:               0x%x\n", header->e_entry);
 }
 
 /**
  * main - entry point
- * @argc: argument count
- * @argv: arguments array
- *
+ * @argc: the number of command-line arguments
+ * @argv: an array of command-line argument strings
  * Return: 0 on success, 98 on failure
  */
 int main(int argc, char **argv)
 {
-    int fd;
-    Elf32_Ehdr header;
-    ssize_t bytes_read;
+	int fd;
+	Elf32_Ehdr header;
 
-    if (argc != 2)
-    {
-        print_error("Invalid number of arguments");
-        return (98);
-    }
+	if (argc != 2)
+		print_error("Invalid number of arguments");
 
-    fd = open(argv[1], O_RDONLY);
-    if (fd == -1)
-    {
-        print_error("Unable to open file");
-        return (98);
-    }
+	fd = open(argv[1], O_RDONLY);
+	if (fd == -1)
+		print_error("Unable to open file");
 
-    bytes_read = read(fd, &header, sizeof(header));
-    if (bytes_read != sizeof(header))
-    {
-        print_error("Unable to read ELF header");
-        close(fd);
-        return (98);
-    }
+	if (read(fd, &header, sizeof(header)) != sizeof(header))
+		print_error("Unable to read ELF header");
 
-    if (header.e_ident[EI_MAG0] != ELFMAG0 ||
-        header.e_ident[EI_MAG1] != ELFMAG1 ||
-        header.e_ident[EI_MAG2] != ELFMAG2 ||
-        header.e_ident[EI_MAG3] != ELFMAG3)
-    {
-        print_error("File is not an ELF file");
-        close(fd);
-        return (98);
-    }
+	if (header.e_ident[EI_MAG0] != ELFMAG0 ||
+	    header.e_ident[EI_MAG1] != ELFMAG1 ||
+	    header.e_ident[EI_MAG2] != ELFMAG2 ||
+	    header.e_ident[EI_MAG3] != ELFMAG3)
+		print_error("File is not an ELF file");
 
-    print_header(&header);
-    close(fd);
+	print_header(&header);
 
-    return (0);
+	close(fd);
+	return (0);
 }
 
