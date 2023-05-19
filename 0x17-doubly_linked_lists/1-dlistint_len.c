4929cb13c070d07e93be38636f331b36c7bfe3f8
chrisdroid <bobitlmr188221@spu.ac.ke>
2023-05-19 13:55:09 +0300

    Implement dlistint_len function

---
+++ 0x17-doubly_linked_lists/1-dlistint_len.c
@@ -0,0 +1,21 @@
+#include "lists.h"
+
+/**
+ * dlistint_len - finds the number of elements in a linked list
+ * @h: given list
+ *
+ * Return: length of @h
+ */
+
+size_t dlistint_len(const dlistint_t *h)
+{
+	size_t elements = 0;
+
+	while (h)
+	{
+		if (h->n)
+			elements++;
+		h = h->next;
+	}
+	return (elements);
+}
