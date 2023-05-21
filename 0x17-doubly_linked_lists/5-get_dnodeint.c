9ab9dd1bdd15f6e751ceb94bbc884db363a621a0
chrisdroid <bobitlmr188221@spu.ac.ke>
2023-05-21 10:39:21 +0300

    Fix bug in code

--- 0x17-doubly_linked_lists/5-get_dnodeint.c
+++ 0x17-doubly_linked_lists/5-get_dnodeint.c
@@ -11,15 +11,13 @@
 dlistint_t *get_dnodeint_at_index(dlistint_t *head, unsigned int index)
 {
 	unsigned int i = 0;
-	dlistint_t *temp = malloc(sizeof(dlistint_t));
-
 	while (head)
 	{
 		if (i == index)
-			temp = head;
+			return (head);
 		head = head->next;
 		i++;
 	}
 
-	return (temp);
+	return NULL;
 }
