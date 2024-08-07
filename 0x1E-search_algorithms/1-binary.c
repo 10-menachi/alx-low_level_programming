24e75f659f7bbd75752f6276d567939457d754a8
chrisdroid <bobitlmr188221@spu.ac.ke>
2024-08-07 14:47:21 +0300

    Implement Binary Search

---
+++ 0x1E-search_algorithms/1-binary.c
@@ -0,0 +1,41 @@
+#include "search_algos.h"
+
+/**
+ * binary_search - Implementation of Binary Search
+ * @array: Provided Array
+ * @size: Size of @array
+ * @value: Provided value
+ *
+ * Return: first index at which @value is located
+ */
+
+int binary_search(int *array, size_t size, int value)
+{
+	size_t l, r, m, i;
+
+	if (!array)
+		return (-1);
+	l = 0;
+	r = size - 1;
+
+	while (l <= r)
+	{
+		printf("Searching in array: ");
+		for (i = l; i <= r; i++)
+		{
+			printf("%d", array[i]);
+			if (i < r)
+				printf(", ");
+		}
+		printf("\n");
+		mid = (l + r) / 2;
+		if (array[m] == value)
+			return (m);
+		if (array[m] < value)
+			l = m + 1;
+		else
+			r = m - 1;
+	}
+
+	return (-1);
+}
